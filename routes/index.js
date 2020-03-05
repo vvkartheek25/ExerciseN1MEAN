157b0714ad168f1a1316a74f9679abc86fb56b6c
Kartheek Vatti <55904811+vvkartheek25@users.noreply.github.com>
2020-03-04 18:13:01 -0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ router.get('/', function(req, res, next) {
 
 //connect with mongodb and fetch records from Users
 var mongodb = require('mongodb');
-const DATABASE_URL = 'mongodb://asharma51:a1s2d3f4@ds215380.mlab.com:15380/heroku_vd9bt3bv';
+const DATABASE_URL = 'mongodb://vvkartheek25@ds329058.mlab.com:29058/heroku_92gxml7b';
 
 router.get('/db', function (request, response, next) {
   mongodb.MongoClient.connect(DATABASE_URL,  function(err, client) {
